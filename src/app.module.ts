e0069538694edd813c10d497ef94f56b5835f421
eriker75 <eribertmarquez75@gmail.com>
2022-10-15 18:35:19 -0400

    Making a docker workflow with make file and mongo db

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,19 +2,18 @@ import { Module } from '@nestjs/common';
 
 import { MongooseModule } from '@nestjs/mongoose';
 import { UsersModule } from './modules/users/users.module';
-import { TicketModule } from './ticket/ticket.module';
 import { AuthorizationModule } from './modules/authorization/authorization.module';
 import { ConfigModule } from '@nestjs/config';
+import { HelloController } from './hello.controller';
 
 @Module({
   imports: [
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/medico'),
-    TicketModule,
     UsersModule,
     AuthorizationModule,
     ConfigModule.forRoot({ isGlobal: true }),
   ],
-  controllers: [],
+  controllers: [HelloController],
   providers: [],
 })
 export class AppModule {}
