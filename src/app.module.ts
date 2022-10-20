c168afc8c06eba931ee8b6f8b84fdc11904923f0
eriker75 <eribertmarquez75@gmail.com>
2022-10-19 21:59:11 -0400

    Docker and swagger initial setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,9 +5,22 @@ import { UsersModule } from './modules/users/users.module';
 import { AuthorizationModule } from './modules/authorization/authorization.module';
 import { ConfigModule } from '@nestjs/config';
 import { HelloController } from './hello.controller';
+import * as Joi from 'joi';
+import { enviroments } from './enviroments';
+import config from './config';
 
 @Module({
   imports: [
+    ConfigModule.forRoot({
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [config],
+      isGlobal: true,
+      validationSchema: Joi.object({
+        API_KEY: Joi.string().required(),
+        DB_NAME: Joi.string().required(),
+        DB_PORT: Joi.number().required(),
+      }),
+    }),
     MongooseModule.forRoot('mongodb://127.0.0.1:27017/medico'),
     UsersModule,
     AuthorizationModule,
